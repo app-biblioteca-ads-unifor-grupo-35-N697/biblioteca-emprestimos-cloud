186655ebaef31c726c18e020ba629b56e8240007
M Daniel  Dantas <contatomarcosdgomes@gmail.com>
2026-03-23 10:46:00 -0300

    atualiza o nome do middleware de tratamento de erros para manter a consistência

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const morgan = require("morgan");
 const authRouter  = require("./routes/auth");
 const apiRouter = require("./routes/api");
-const errorMiddleware = require("./middlewares/error-middleware");
+const errorHandler = require("./middlewares/error-handler");
 const { swaggerUi, swaggerSpec } = require("./swagger");
 
 const app = express();
@@ -13,6 +13,6 @@ app.use(express.json());
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/auth", authRouter);
 app.use("/api", apiRouter);
-app.use(errorMiddleware);
+app.use(errorHandler);
 
 module.exports = app;
