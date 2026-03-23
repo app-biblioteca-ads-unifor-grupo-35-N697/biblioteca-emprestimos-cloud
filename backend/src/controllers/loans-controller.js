757d3bb921cc423870d52f132c97ee9a6522a8f6
M Daniel  Dantas <contatomarcosdgomes@gmail.com>
2026-03-23 10:27:50 -0300

    adiciona tratamento de erros nas rotas do controlador de empréstimos

--- backend/src/controllers/loans-controller.js
+++ backend/src/controllers/loans-controller.js
@@ -4,37 +4,53 @@ const loansModel = require("../models/loans-model")
 
 module.exports = {
  // GET   /api/loans ---busca todos os emprestimos
- index: async (req, res) => {
-    const loans = await loansModel.getAllLoans()
-    res.json(loans)
+ index: async (req, res, next) => {
+    try {
+      const loans = await loansModel.getAllLoans()
+      res.json(loans)
+    } catch (error) {
+      next(error)
+    }
  },
 
  // GET   /api/loans/:id ---busca um emprestimo por id
- show: async (req, res) => {
-  const { id } = req.params
-  const loan = await loansModel.getLoanById(id)
-  if (!loan) throw new HttpError(404, "emprestimo nao encontrado")
-  res.json(loan)
+ show: async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const loan = await loansModel.getLoanById(id)
+    if (!loan) throw new HttpError(404, "emprestimo nao encontrado")
+    res.json(loan)
+  } catch (error) {
+    next(error)
+  }
  },
 
  // POST  /api/loans ---cria um novo emprestimo
- save: async (req, res) => {
-   const user = req.user
-   const { bookId } = req.body
+ save: async (req, res, next) => {
+   try {
+     const user = req.user
+     const { bookId } = req.body
 
-   if (typeof bookId !== 'string') throw new HttpError(400, 'ID de livro inválido!')
+     if (typeof bookId !== 'string') throw new HttpError(400, 'ID de livro inválido!')
 
-   const book = await booksModel.getBookById(bookId)
-   if (!book) throw new HttpError(404, 'Livro não encontrado!')
+     const book = await booksModel.getBookById(bookId)
+     if (!book) throw new HttpError(404, 'Livro não encontrado!')
 
-   const newLoan = await loansModel.createLoan(user, book)
-   res.status(201).json(newLoan)
+     const newLoan = await loansModel.createLoan(user, book)
+     res.status(201).json(newLoan)
+   } catch (error) {
+     next(error)
+   }
  },
 
   // POST /api/loans/:id/return
-  return: async (req, res) => {
-   const { id } = req.params
-   const loan = await loansModel.returnLoan(id)
-   res.json(loan)
+  return: async (req, res, next) => {
+   try {
+     const { id } = req.params
+     const loan = await loansModel.returnLoan(id)
+     res.json(loan)
+   } catch (error) {
+     next(error)
+   }
  }
 }
